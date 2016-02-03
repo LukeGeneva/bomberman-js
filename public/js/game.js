5c78aa952ae022215e3248967aac654826fd6f70
lukegeneva <lukegeneva@gmail.com>
2016-02-02 23:11:55 -0600

    movement testing code

--- public/js/game.js
+++ public/js/game.js
@@ -4,10 +4,32 @@ function preload() {
 
 }
 
-function create() {
+var testSprite;
 
+function create() {
+	// bitmap data to represent test player
+	var bmd = game.add.bitmapData(30, 30);
+	bmd.ctx.beginPath();
+	bmd.ctx.rect(0,0,30,30);
+	bmd.ctx.fillStyle = '#ff0000';
+	bmd.ctx.fill();
+
+	testSprite = game.add.sprite(200, 200, bmd);
 }
 
+var testSpeed = 4;
+
 function update() {
-	
+	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+		testSprite.x -= testSpeed;
+	}
+	else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+		testSprite.x += testSpeed;
+	}
+	else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+		testSprite.y -= testSpeed;
+	}
+	else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+		testSprite.y += testSpeed;
+	}
 }
