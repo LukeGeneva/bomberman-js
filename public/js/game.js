4259586bd683ce2a9eba71d26eb393223968da8c
LukeGeneva <lukegeneva@gmail.com>
2016-02-10 03:29:48 -0600

    Added helper functions to assist with grid navigation.
    Game now assists players with walking between tiles by pushing them in
    the correct direction if they are within a threshold value of the middle
    of an open tile.
    
    Increased bomber body height to improve navigation as well.

--- public/js/game.js
+++ public/js/game.js
@@ -52,15 +52,19 @@ function handleMoveRequest(sender, direction) {
 	switch (direction) {
 		case 'north':
 			player.body.velocity.y = -player.speed;
+			player.body.velocity.x = calculateHelperXVelocity();
 			break;
 		case 'south':
 			player.body.velocity.y = player.speed;
+			player.body.velocity.x = calculateHelperXVelocity();
 			break;
 		case 'east':
 			player.body.velocity.x = player.speed;
+			player.body.velocity.y = calculateHelperYVelocity();
 			break;
 		case 'west':
 			player.body.velocity.x = -player.speed;
+			player.body.velocity.y = calculateHelperYVelocity();
 			break;
 	}
 }
@@ -71,11 +75,61 @@ function handleStopRequest(sender) {
 }
 
 function handleBombDropRequest(sender) {
-	var bombX = Math.floor(player.body.center.x / 16) * 16 + 8;
-	var bombY = Math.floor(player.body.center.y / 16) * 16 + 8;
+	var bombX = Math.floor(player.body.center.x / map.tileWidth) * map.tileWidth + map.tileWidth / 2;
+	var bombY = Math.floor(player.body.center.y / map.tileHeight) * map.tileHeight + map.tileHeight / 2;
 	var bomb = player.dropBomb();
 	bombs.add(bomb);
 	bomb.x = bombX;
 	bomb.y = bombY;
 	bomb.startFuse();
+}
+
+function calculateHelperXVelocity() {
+	var playerTile = map.getTileWorldXY(player.body.center.x, player.body.center.y);
+	var destinationTile;
+	if (player.body.blocked.up) {
+		destinationTile = map.getTileAbove(map.getLayer(fixedLayer), playerTile.x, playerTile.y);
+	}
+	else if (player.body.blocked.down) {
+		destinationTile = map.getTileBelow(map.getLayer(fixedLayer), playerTile.x, playerTile.y);
+	}
+	else {
+		return 0;
+	}
+
+	if (destinationTile.index !== -1) return 0;
+
+	var playerTileCenter = playerTile.left + playerTile.width / 2;
+	var helperThreshold = 8;
+	var offset = playerTileCenter - player.body.center.x;
+	var absoluteOffset = Math.abs(offset);
+	if (absoluteOffset === 0 || absoluteOffset > helperThreshold) {
+		return 0;
+	}
+	return offset > 0 ? player.speed : -player.speed;
+}
+
+function calculateHelperYVelocity() {
+	var playerTile = map.getTileWorldXY(player.body.center.x, player.body.center.y);
+	var destinationTile;
+	if (player.body.blocked.left) {
+		destinationTile = map.getTileLeft(map.getLayer(fixedLayer), playerTile.x, playerTile.y);
+	}
+	else if (player.body.blocked.right) {
+		destinationTile = map.getTileRight(map.getLayer(fixedLayer), playerTile.x, playerTile.y);
+	}
+	else {
+		return 0;
+	}
+
+	if (destinationTile.index !== -1) return 0;
+
+	var playerTileCenter = playerTile.top + playerTile.height / 2;
+	var helperThreshold = 8;
+	var offset = playerTileCenter - player.body.center.y;
+	var absoluteOffset = Math.abs(offset);
+	if (absoluteOffset === 0 || absoluteOffset > helperThreshold) {
+		return 0;
+	}
+	return offset > 0 ? player.speed : -player.speed;
 }
