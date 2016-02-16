bda4afc45966dcaedf7b73a3d17741750dd6f592
LukeGeneva <lukegeneva@gmail.com>
2016-02-16 02:31:37 -0600

    Added jshint to gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 
-gulp.task('default', function() {
-
+gulp.task('jshint', function() {
+    gulp.src('./js/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
+
+gulp.task('default', ['jshint']);
