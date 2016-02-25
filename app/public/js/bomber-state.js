445969f6ac04f60d7d33450b4cb1555c7883f728
LukeGeneva <lukegeneva@gmail.com>
2016-02-25 03:42:34 -0600

    Initial code to implement bomber state machine.

---
+++ app/public/js/bomber-state.js
@@ -0,0 +1,21 @@
+var BomberState = (function() {
+    'use strict';
+
+    function BomberState(bomber) {
+        this.update = function(input) {
+
+        }
+    });
+})();
+
+var BomberStandingState = (function() {
+    'use strict';
+
+    function BomberStandingState(bomber) {
+        this.update = function(input) {
+
+        }
+    }
+
+    return BomberStandingState;
+})();
