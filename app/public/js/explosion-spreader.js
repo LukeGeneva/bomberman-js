21d423111db7775844792d1d7e0358dbe78fc53d
LukeGeneva <lukegeneva@gmail.com>
2016-02-27 03:55:58 -0600

    Explosions spread in all directions.

--- app/public/js/explosion-spreader.js
+++ app/public/js/explosion-spreader.js
@@ -2,23 +2,60 @@ var ExplosionSpreader = (function() {
     'use strict';
     
     function ExplosionSpreader(game, originTile, radius) {
+        var currentTile = originTile;
+        var explosionDistance = radius;
 
         this.spread = function() {
-            createExplosion(originTile);
-            spreadNorth(originTile, radius);
+            createExplosion(currentTile);
+            spreadNorth();
+            spreadSouth();
+            spreadEast();
+            spreadWest();
         };
 
-        var spreadNorth = function(currentTile, explosionDistance) {
-            var nextTile = game.map.getTileAbove(game.map.baseTileLayer.index, currentTile.x, currentTile.y);
-            if (explosionDistance > 0 && !game.map.isTileFixed(nextTile.x, nextTile.y)) {
-                createExplosion(nextTile);
-                spreadNorth(nextTile, explosionDistance-1);
+        var spreadNorth = function() {
+            currentTile = game.map.getTileAbove(game.map.baseTileLayer.index, currentTile.x, currentTile.y);
+            continueSpread(spreadNorth);
+        };
+
+        var spreadSouth = function() {
+            currentTile = game.map.getTileBelow(game.map.baseTileLayer.index, currentTile.x, currentTile.y);
+            continueSpread(spreadSouth);
+        };
+
+        var spreadEast = function() {
+            currentTile = game.map.getTileRight(game.map.baseTileLayer.index, currentTile.x, currentTile.y);
+            continueSpread(spreadEast);
+        };
+
+        var spreadWest = function() {
+            currentTile = game.map.getTileLeft(game.map.baseTileLayer.index, currentTile.x, currentTile.y);
+            continueSpread(spreadWest);
+        };
+
+        var continueSpread = function(spreadFunction) {
+            if (explosionCanSpread()) {
+                createExplosion();
+                explosionDistance--;
+                spreadFunction();
+            }
+            else {
+                resetVars();
             }
         };
 
-        var createExplosion = function(tile) {
+        var explosionCanSpread = function() {
+            return !game.map.isTileFixed(currentTile.x, currentTile.y) && explosionDistance > 0;
+        };
+
+        var createExplosion = function() {
             var explosion = new Explosion(game);
-            game.map.centerSpriteInTile(explosion, tile);
+            game.map.centerSpriteInTile(explosion, currentTile);
+        };
+
+        var resetVars = function() {
+            currentTile = originTile;
+            explosionDistance = radius;
         };
 
     }
